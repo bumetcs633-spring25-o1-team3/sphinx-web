848c7d8b97d6e8bce44ba701372da0ca08d96eb6
vishal <vishal2@bu.edu>
2025-02-15 16:05:30 +0530

    add sign in with google link

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'preact-router';
 
 const App = () => {
+  const backendUrl = import.meta.env.VITE_API_URL;
   return (
     <div>
       <h1>Welcome to Preact + Vite</h1>
       <nav>
         <Link href="/">Home</Link>
         <Link href="/quizzes">Quizzes</Link>
+        <a href={`${backendUrl}/oauth2/authorization/google`}>Sign in with Google</a>
       </nav>
     </div>
   );
